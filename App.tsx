473f3ff2bfdd4890aa941559326d5508eaf9966f
Meskour <sotradia@gmail.com>
2022-05-17 19:54:04 +0100

    Setup tailwind css

--- App.tsx
+++ App.tsx
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
+import { useTailwind } from 'tailwind-rn';
+import {TailwindProvider} from 'tailwind-rn';
+import utilities from './tailwind.json';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  
+
+  const MyComponent = () => {
+    const tailwind = useTailwind();
+
+    return <Text style={tailwind('text-blue-600')}>Hello world!</Text>;
+  };
+
+  return (<TailwindProvider utilities={utilities}>
+     <MyComponent/>
+   </TailwindProvider> );
+  // return (
+  //   <TailwindProvider utilities={utilities}>
+  //     <SafeAreaView style={tailwind('h-full')}>
+        
+  //       <View style={tailwind('items-center pt-12')}>
+  //         <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
+  //           <Text style={tailwind('text-red-100')}>Hello world!</Text>
+  //         </View>    
+  //         <StatusBar style="auto" />
+  //       </View>
+
+  //     </SafeAreaView>
+  //   </TailwindProvider>
+  // );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
